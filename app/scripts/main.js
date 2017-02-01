32dd097b9b1ba1a236e84a3e597bab3cc6dbce4d
Patrick Ratigan <patrickratigan@patricks-mbp.home>
2017-01-31 19:34:59 -0500

    modify styles

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,20 @@
 (function($){
   "use strict";
 
+//hide all list items on page load
+  $("h3").siblings().hide();
+
+//add h3 click event
   $("h3").on("click", function() {
-      if ($(this).siblings().hasClass('hidden')) {
-
-        // make sure all other lists are hidden
-        $(this).parents().siblings().children('ul').addClass('hidden');
-
-        $(this).siblings().removeClass('hidden');
-      } else {
-        $(this).siblings().addClass('hidden');
-
+    //if list items are visible
+      if ($(this).siblings().is(":visible")) {
+    //hide list items;
+  $(this).siblings().slideUp("slow");
+}else{
+  //show list items
+$(this).siblings().slideDown("slow");
+//hide all other list items
+$(this).parents().siblings().children('ul').slideUp('slow');
       }
   });
 
